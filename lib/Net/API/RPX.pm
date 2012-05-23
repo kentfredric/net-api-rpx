da5c0bb000e97d36cb6882c58c5a507a93908a99
Kent Fredric <kentfredric@gmail.com>
2012-05-24 11:30:21 +1200

    0.02 release

--- lib/Net/API/RPX.pm
+++ lib/Net/API/RPX.pm
@@ -14,11 +14,11 @@ Net::API::RPX - Perl interface to Janrain's RPX service
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 has api_key => (
     is       => 'rw',
